a59e963af8f7a52acf4a09389669424f15e122e9
RaviChaudhary <ravichaudhary3991@gmail.com>
2020-12-06 20:06:15 +0530

    modified mainArgs.c

--- local/mainArgs.c
+++ local/mainArgs.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
-extern char** environ;
 int main(int argc, char* argv[], char** env)
 {
 	printf("%d\n", argc);
 	for(int i=0; i<argc; i++){
 		printf("%s ", argv[i]);
 	}
-	printf("\nenvironment\n");
-	for(int i=0; environ[i] != NULL; i++){
-		printf("%s ", environ[i]);
+	printf("\nenvment\n");
+	for(int i=0; env[i] != NULL; i++){
+		printf("%s ", env[i]);
 	for(int i=0; env[i] != NULL; i++){
 		printf("%s ", env[i]);
 	}
