c51f3f9d3b81eebe2405ac4cb2cc7572aa11a769
RaviChaudhary <ravichaudhary3991@gmail.com>
2020-12-06 19:06:23 +0530

    local branch is added mainArgs.c is modified

---
+++ local/mainArgs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+extern char** environ;
+int main(int argc, char* argv[], char** env)
+{
+	printf("%d\n", argc);
+	for(int i=0; i<argc; i++){
+		printf("%s ", argv[i]);
+	}
+	printf("\nenvironment\n");
+	for(int i=0; environ[i] != NULL; i++){
+		printf("%s ", environ[i]);
+	}
+	return 0;
+}
