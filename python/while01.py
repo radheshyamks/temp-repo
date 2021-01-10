ebb2bc2642fc8039dbd65cc230e94890bd2109e1
radheshyamks <radhehsyams51@gmail.com>
2021-01-10 13:09:28 +0530

    while loop program is added

---
+++ python/while01.py
@@ -0,0 +1,8 @@
+x = int(input("enter the value: "))
+while(True):
+    if(x == 0):
+        print("while loop is broken")
+        break
+    else:
+        print("Enter valid number 0: ")
+        x = int(input())
