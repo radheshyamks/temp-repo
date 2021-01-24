fe923981a67f22f933618def34bec4e2cedf760f
radheshyamks <radhehsyams51@gmail.com>
2021-01-24 15:11:23 +0530

    Added smart pointer in C++

---
+++ cpp/smartptr/self.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+class abc{
+	private:
+		int* data;
+	public:
+		abc(int *ptr= nullptr): data{ptr}{std::cout<<"this is constructor"<<std::endl;}
+		~abc(){delete data; std::cout<<"this is destructor"<<std::endl;}
+		int& operator* (){
+			std::cout<<"this is overloaded function"<<std::endl;
+			return *data;
+		}
+};
+int main()
+{
+	int* ptr = new int(100);
+	//abc obj = abc(ptr);
+	abc obj(ptr);
+	std::cout<<"after calling constructor"<<std::endl;
+	std::cout<<"value obj = "<< *obj<<std::endl;
+	std::cout<<"*ptr = "<<*ptr<<std::endl;
+	return 0;
+}
