f4dcac45a807f2552fd61cb8c5ad37e95587a0bf
radheshyamks <radhehsyams51@gmail.com>
2021-02-18 22:16:47 +0530

    Added rough folder in C

---
+++ C/rough/bool.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>/* if this header will not be included then it will throw error*/
+#include <stdio.h>
+int main(){
+	bool flag1 = true;
+	bool flag2 = false;
+	printf("%d\n", flag1);
+	return 0;
+}
