de6a52385a7c9c17a6681e2db570a3ba335e1b77
RadheShyam <RadheShyams51@gmail.com>
2024-10-26 19:17:32 +0530

    Master branch

--- Cpp/Hello.cpp
+++ Cpp/Hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(int argc, char* argv[], char** env){
     std::cout<<"Hello World\n";
+    //Edited for merging.
     // EXIT_SUCCESS is a macro which is having value as 0
     return EXIT_SUCCESS;
 }
