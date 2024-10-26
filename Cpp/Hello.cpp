2ea9cfd3f83d882a7a470b61bdaba7e4bb9a7b54
RadheShyam <RadheShyams51@gmail.com>
2024-10-26 19:02:18 +0530

    modified hello.cpp

--- Cpp/Hello.cpp
+++ Cpp/Hello.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main(int argc, char* argv[], char** env){
     std::cout<<"Hello World\n";
+    // EXIT_SUCCESS is a macro which is having value as 0
     return EXIT_SUCCESS;
 }
